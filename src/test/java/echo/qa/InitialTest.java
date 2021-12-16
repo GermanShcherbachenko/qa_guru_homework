c8e343c170287d5dd0d87b502ce84b15851e5831
Герман Щербаченко <scherbachenko.ga@unicom24.ru>
2021-12-16 15:48:01 +0300

    complited filling the form

--- src/test/java/echo/qa/InitialTest.java
+++ src/test/java/echo/qa/InitialTest.java
@@ -1,10 +1,16 @@
 package echo.qa;
 
-import static com.codeborne.selenide.Selenide.open;
-
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import io.netty.util.concurrent.NonStickyEventExecutorGroup;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class InitialTest {
 
@@ -16,6 +22,29 @@ public class InitialTest {
     @Test
     void firstTest() {
         open("https://demoqa.com/automation-practice-form");
-        System.out.println("test");
+        $(".main-header").shouldHave(Condition.text("Practice Form"));
+        $("#firstName").setValue("Николай");
+        $("#lastName").setValue("Иванов");
+        $("#userEmail").setValue("randomEmail@gmail.com");
+        $(byText("Male")).click();
+        $("#userNumber").setValue("9998887766");
+        $("#dateOfBirthInput").sendKeys(Keys.CONTROL + "a");
+        $("#dateOfBirthInput").sendKeys("08 Jun 1989", Keys.ENTER);
+        $("#subjectsInput").sendKeys("Hindi");
+        $("#subjectsInput").pressEnter();
+        $(byText("Sports")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/java/echo/qa/resources/file.txt"));
+        $("#currentAddress").setValue("Рандомная улица в случайном городе");
+        $("#react-select-3-input").sendKeys("NCR");
+        $("#react-select-3-input").pressEnter();
+        $("#react-select-4-input").sendKeys("Noida");
+        $("#react-select-4-input").pressEnter();
+        $("#submit").click();
+
+
+
+
+
+
     }
 }
